fe2cd931de27dfd3e04340daf4eb8455f710cfd9
Dylan Mora <damora@uamv.edu.ni>
2025-10-15 13:23:22 -0600

    suma

---
+++ suma/suma/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace suma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1 = 2;
+            int numero2 = 3;
+            int suma = numero1 + numero2;
+            Console.WriteLine(suma);
+        }
+    }
+}
